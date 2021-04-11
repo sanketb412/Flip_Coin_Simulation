807360dd3372bd7234e870dc7893a7e182d9aa93
Shweta Singh <Shweta.Singh1@Nykaa.local>
2021-04-11 21:30:56 +0530

    Added: code for Number of Head and Tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,22 @@
 
 echo "# Flip_Coin_Simulation"
 
-#UC1
 temp=1
-rand=$((RANDOM%2))
+count=1
+Head=0
+Tail=0
 
-if [ $temp -eq $rand ];
-then
-	echo "Heads*"
-else
-	echo "Tails*"
-fi
+while [ $count != 10 ]
+do
+	rand=$((RANDOM%2))
+
+	if [ $temp -eq $rand ];
+	then
+		Head=$(($Head + 1))
+		echo "Heads* got won $Head Times"
+	else
+		Tail=$(($Tail + 1))
+		echo "Tails* got won $Tail TImes"
+	fi
+	((count++))
+done
